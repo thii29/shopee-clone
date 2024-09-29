1372943606ab0a5b760275ed6ce35fcd9f106b22
Thi Pham <thipham1123@gmail.com>
2024-09-29 22:02:04 +0700

    yup register

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -2,19 +2,25 @@ import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import Input from 'src/components/Input'
 import { getRules } from 'src/utils/rules'
+import { schema, Schema } from 'src/utils/rules'
+import { yupResolver } from '@hookform/resolvers/yup'
 
-interface FormData {
-  email: string
-  password: string
-  confirm_password: string
-}
+// interface FormData {
+//   email: string
+//   password: string
+//   confirm_password: string
+// }
+type FormData = Schema
+
 export default function Register() {
   const {
     register,
     handleSubmit,
     getValues,
     formState: { errors }
-  } = useForm<FormData>()
+  } = useForm<FormData>({
+    resolver: yupResolver(schema)
+  })
   const rules = getRules(getValues)
   const onSubmit = handleSubmit((data) => {
     console.log(data)
@@ -35,7 +41,7 @@ export default function Register() {
                 className='mt-8'
                 errorMessage={errors.email?.message}
                 placeHolder='Email'
-                rules={rules.email}
+                //rules={rules.email}
               />
               {/* <div className='mt-8'>
                 <input
@@ -55,7 +61,7 @@ export default function Register() {
                 className='mt-2'
                 errorMessage={errors.password?.message}
                 placeHolder='Password'
-                rules={rules.password}
+                //rules={rules.password}
                 autoComplete='on'
               />
               <Input
@@ -65,7 +71,7 @@ export default function Register() {
                 className='mt-2'
                 errorMessage={errors.confirm_password?.message}
                 placeHolder='Confirm Password'
-                rules={rules.confirm_password}
+                //rules={rules.confirm_password}
                 autoComplete='on'
               />
               <div className='mt-2'>
--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -1,10 +1,10 @@
-
 import { UseFormGetValues } from 'react-hook-form'
+import * as yup from 'yup'
 
 //type Rules = { [key in 'email' | 'password' | 'confirm_password']?: RegisterOptions | undefined }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const getRules = (getValues?: UseFormGetValues<any>)/*: Rules*/ => ({
+export const getRules = (getValues?: UseFormGetValues<any>) /*: Rules*/ => ({
   email: {
     required: {
       value: true,
@@ -30,11 +30,11 @@ export const getRules = (getValues?: UseFormGetValues<any>)/*: Rules*/ => ({
     },
     maxLength: {
       value: 160,
-      message: 'Length of email from 6 - 160 characters'
+      message: 'Length of password from 6 - 160 characters'
     },
     minLength: {
       value: 6,
-      message: 'Length of email from 6 - 160 characters'
+      message: 'Length of password from 6 - 160 characters'
     }
   },
   confirm_password: {
@@ -44,15 +44,38 @@ export const getRules = (getValues?: UseFormGetValues<any>)/*: Rules*/ => ({
     },
     maxLength: {
       value: 160,
-      message: 'Length of email from 6 - 160 characters'
+      message: 'Length of password from 6 - 160 characters'
     },
     minLength: {
       value: 6,
-      message: 'Length of email from 6 - 160 characters'
+      message: 'Length of password from 6 - 160 characters'
     },
     validate:
       typeof getValues === 'function'
-        ? (value ?:string) => (value === getValues('password') ? true : 'Confirm password does not match')
+        ? (value?: string) => (value === getValues('password') ? true : 'Confirm password does not match')
         : undefined
   }
 })
+
+export const schema = yup.object({
+  email: yup
+    .string()
+    .required('This field is required')
+    .email('Incorrect email format')
+    .min(5, 'Length of email from 5 - 160 characters')
+    .max(160, 'Length of email from 5 - 160 characters'),
+  password: yup
+    .string()
+    .required('This field is required')
+    .min(6, 'Length of password from 6 - 160 characters')
+    .max(6, 'Length of password from 6 - 160 characters'),
+  confirm_password: yup
+    .string()
+    .required('This field is required')
+    .min(6, 'Length of password from 6 - 160 characters')
+    .max(6, 'Length of password from 6 - 160 characters')
+    .oneOf([yup.ref('password')], 'Confirm password does not match')
+
+})
+
+export type Schema = yup.InferType<typeof schema>
