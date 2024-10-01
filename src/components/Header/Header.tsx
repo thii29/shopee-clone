d73304075cbf09595eebae4fba0be716c2fa26e1
Thi Pham <thipham1123@gmail.com>
2024-10-01 22:34:21 +0700

    error popover

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom'
+import Popover from '../Popover '
 
 export default function Header() {
   return (
     <div className='pb-5 pt-2 bg-gradient-to-t from-[#f53d2d] to-[#ff6633] text-white'>
       <div className='container'>
         <div className='flex justify-end'>
+          <Popover>
+            <span className='mx-1'>Vietnamese</span>
+          </Popover>
           <div className='flex items-center py-1 hover:text-gray-300 cursor-pointer'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
@@ -20,7 +24,7 @@ export default function Header() {
                 d='M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418'
               />
             </svg>
-            <div className='mx-1'>Vietnamese</div>
+            <div className='mx-1'>English</div>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               fill='none'
