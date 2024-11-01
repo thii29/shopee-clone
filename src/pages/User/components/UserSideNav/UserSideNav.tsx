b50cc481067c5380b3cfea6b80bad7709ab61064
Thi Pham <thipham1123@gmail.com>
2024-11-01 16:10:14 +0700

    ui side nav in profile user

--- src/pages/User/components/UserSideNav/UserSideNav.tsx
+++ src/pages/User/components/UserSideNav/UserSideNav.tsx
@@ -1,6 +1,94 @@
+import { Link } from 'react-router-dom'
+import path from 'src/constants/path'
 
 export default function UserSideNav() {
   return (
-    <div>UserrSideNav</div>
+    <div>
+      <div className='flex items-center border-b border-b-gray-200 py-4'>
+        <Link to={path.profile} className='h-12 w-12 flex-shrink-0 overflow-hidden rounded-full border border-black/10'>
+          <img
+            src='https://down-vn.img.susercontent.com/file/vn-11134233-7r98o-lleidcjlrkqw44_tn'
+            alt=''
+            className='h-full w-full object-cover'
+          />
+        </Link>
+        <div className='flex-grow pl-4'>
+          <div className='mb-1 truncate font-semibold text-gray-600'>thii298</div>
+          <Link to={path.profile} className='flex items-center capitalize text-gray-500'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='size-4 mr-2'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125'
+              />
+            </svg>{' '}
+            Sửa hồ sơ
+          </Link>
+        </div>
+      </div>
+      <div className='mt-7'>
+        {/* my account */}
+        <Link to={path.profile} className='flex items-center capitalize text-orange transition-colors'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='size-5 mr-1'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z'
+            />
+          </svg>
+          Tài khoản của tôi
+        </Link>
+        {/* change password */}
+        <Link to={path.changePassword} className='flex items-center capitalize text-gray transition-colors my-2'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='size-5 mr-1'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10'
+            />
+          </svg>
+          Đổi mật khẩu
+        </Link>
+        {/*history buy*/}
+        <Link to={path.profile} className='flex items-center capitalize text-gray transition-colors my-2'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='size-5 mr-1'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75'
+            />
+          </svg>
+          Lịch sử mua hàng
+        </Link>
+      </div>
+    </div>
   )
 }
